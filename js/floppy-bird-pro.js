af2e4479451742c3bf94c5ea5359138289cc4976
Mr. Daks <DRAVKNOX@gmail.com>
2026-06-30 17:12:00 +0530

    Delete js directory

--- js/floppy-bird-pro.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const hint = document.querySelector('.hint');
-  if (!hint) return;
-  let up = true;
-  setInterval(() => {
-    hint.style.opacity = up ? '1' : '.72';
-    up = !up;
-  }, 900);
-})();
